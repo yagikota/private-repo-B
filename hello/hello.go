da62a5587bd16aab4dd5a1ed2c62bae52bf8b6ac
Kota <mdryzk64smsh@gmail.com>
2023-05-04 19:38:52 +0900

    add hello2

--- hello/hello.go
+++ hello/hello.go
@@ -1,5 +1,11 @@
 package hello
 
-func hello(s string) string {
-	return "hello" + s
+import "fmt"
+
+func hello1(s string) string {
+	return fmt.Sprintf("func hello1: arg=%s", s)
+}
+
+func hello2(s string) string {
+	return fmt.Sprintf("func hello2: arg=%s", s)
 }
